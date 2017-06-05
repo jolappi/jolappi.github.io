070633df912036b80f39587b796b6de2d5253b39
Jone73 <jone@stainedblood.com>
2017-06-05 07:55:23 +0300

    refire conf

---
+++ refire.js
@@ -0,0 +1,16 @@
+<script>
+  var config = {
+    apiKey: "AIzaSyAvzTNPrudlKMhuE8d5sbp0IbrItqR2MTE",
+
+    // Only needed if using Firebase Realtime Database (which we will be in this example)
+    databaseURL: "https://messageinthebottle-4d3dc.firebaseio.com",
+
+    // Only needed if using Firebase Authentication
+    authDomain: "<YOUR-AUTH-DOMAIN>",
+
+    // Only needed if using Firebase Storage
+    storageBucket: "<YOUR-STORAGE-BUCKET>.appspot.com"
+  };
+
+  firebase.initializeApp(config);
+</script>
