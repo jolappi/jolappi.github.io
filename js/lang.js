50f38340341c2ecaee7f84c1e39bf48e59fcc257
jolappi <jone@stainedblood.com>
2017-08-15 13:13:26 +0300

    upod4

--- js/lang.js
+++ js/lang.js
@@ -2,6 +2,32 @@ var langs = ['en', 'fi'];
 var langCode = '';
 var langJS = null;
 
+var getFirstBrowserLanguage = function () {
+    var nav = window.navigator,
+    browserLanguagePropertyKeys = ['language', 'browserLanguage', 'systemLanguage', 'userLanguage'],
+    i,
+    language;
+
+    // support for HTML 5.1 "navigator.languages"
+    if (Array.isArray(nav.languages)) {
+      for (i = 0; i < nav.languages.length; i++) {
+        language = nav.languages[i];
+        if (language && language.length) {
+          return language;
+        }
+      }
+    }
+
+    // support for other well known properties in browsers
+    for (i = 0; i < browserLanguagePropertyKeys.length; i++) {
+      language = nav[browserLanguagePropertyKeys[i]];
+      if (language && language.length) {
+        return language;
+      }
+    }
+
+    return "en";
+  };
 
 var translate = function (data)
 {
@@ -14,8 +40,8 @@ var translate = function (data)
 }
 
 
-langCode = navigator.language.substr (0, 2);
-console.log(langCode);
+langCode = getFirstBrowserLanguage();
+console.log(langCode + window.language);
 if (langCode in langs){
 	$.getJSON('lang/'+langCode+'.json', translate);
 } else {
